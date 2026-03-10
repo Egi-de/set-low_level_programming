b1e4b66a5fe08e95aa2f6d8def495204e764e6bb
Egi-de <egidefiston@gmail.com>
2026-03-10 11:59:59 +0200

    Add 0x05 pointers arrays strings tasks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string in place
+ * @s: pointer to the string to reverse
+ */
+void rev_string(char *s)
+{
+	int len;
+	int i;
+	char tmp;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	i = 0;
+	while (i < len / 2)
+	{
+		tmp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
+		i++;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+# 0x05. C - Pointers, arrays and strings
+
+## Description
+
+This project covers pointers, arrays and strings in C.
+
+## Author
+Egi-de
