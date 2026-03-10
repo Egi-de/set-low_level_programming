145d5114763eb2fd01aa3aa343ef4230bce15fec
Egi-de <egidefiston@gmail.com>
2026-03-10 11:52:09 +0200

    Add 0x04 more functions nested loops tasks

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	i = 1;
+	while (i <= size)
+	{
+		j = 0;
+		while (j < size - i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		j = 0;
+		while (j < i)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+# 0x04. C - More functions, more nested loops
+
+## Description
+
+This project covers writing more functions and using nested loops in C.
+
+## Author
+Egi-de
