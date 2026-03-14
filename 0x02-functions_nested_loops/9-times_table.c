e89050915a283146c60465ed112011096dc76ae3
Egi-de <egidefiston@gmail.com>
2026-03-14 18:15:30 +0200

    Add 0x02 functions nested loops tasks

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			product = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				if (product < 10)
+					_putchar(' ');
+				_putchar(' ');
+			}
+			if (product >= 10)
+				_putchar('0' + product / 10);
+			_putchar('0' + product % 10);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+# 0x02. C - Functions, nested loops
+
+## Description
+
+This project covers functions and nested loops in C.
+
+## Author
+Egi-de
