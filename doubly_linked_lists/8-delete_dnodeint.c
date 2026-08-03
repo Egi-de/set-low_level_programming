bd3b805ae60f5fe0fc402662d40d024be5f07012
Egi-de <egidefiston@gmail.com>
2026-08-03 10:36:42 +0200

    Add doubly linked lists tasks 0-8 and palindrome result

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
+ * @head: pointer to pointer to the head of the list
+ * @index: index of the node to delete
+ * Return: 1 if succeeded, -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+	tmp = *head;
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+	i = 0;
+	while (tmp != NULL && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp == NULL)
+		return (-1);
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+	free(tmp);
+	return (1);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,8 @@
+# Doubly Linked Lists
+
+## Description
+
+This project covers doubly linked list operations in C.
+
+## Author
+Egi-de
