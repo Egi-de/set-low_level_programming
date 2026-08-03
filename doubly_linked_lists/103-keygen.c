2ba2d236973aff7d50115904699391a2348af773
Egi-de <egidefiston@gmail.com>
2026-08-03 11:40:17 +0200

    Split keygen into 3 short functions to satisfy 40-line rule

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,6 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char charset[] =
+	"A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+/**
+ * char_stats - computes sum, product, sum of squares, and max of chars
+ * @name: the username
+ * @len: length of the username
+ * @sum: output - sum of chars
+ * @prod: output - product of chars
+ * @sq: output - sum of squares of chars
+ * @max: output - max char value
+ */
+void char_stats(char *name, int len, int *sum, int *prod, int *sq, int *max)
+{
+	int i;
+
+	*sum = 0;
+	*prod = 1;
+	*sq = 0;
+	*max = (signed char)name[0];
+	for (i = 0; i < len; i++)
+	{
+		*sum += (signed char)name[i];
+		*prod *= (signed char)name[i];
+		*sq += (signed char)name[i] * (signed char)name[i];
+		if ((signed char)name[i] > *max)
+			*max = (signed char)name[i];
+	}
+}
+
+/**
+ * burn_rand - advances the rand() sequence c times
+ * @c: number of times to call rand()
+ * Return: the last value returned by rand()
+ */
+int burn_rand(int c)
+{
+	int r = 0, i;
+
+	for (i = 0; c > i; i++)
+		r = rand();
+	return (r);
+}
+
 /**
  * main - generates a valid crackme5 key for a given username
  * @argc: argument count
@@ -10,11 +54,9 @@
  */
 int main(int argc, char **argv)
 {
-	const char charset[] =
-		"A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char *name;
 	char key[7];
-	int len, i, sum, prod, sq, max, r;
+	int len, sum, prod, sq, max;
 
 	if (argc != 2)
 	{
@@ -23,32 +65,17 @@ int main(int argc, char **argv)
 	}
 	name = argv[1];
 	len = strlen(name);
+	char_stats(name, len, &sum, &prod, &sq, &max);
 
 	key[0] = charset[(len ^ 0x3b) & 0x3f];
-
-	for (sum = 0, i = 0; i < len; i++)
-		sum += (signed char)name[i];
 	key[1] = charset[(sum ^ 0x4f) & 0x3f];
-
-	for (prod = 1, i = 0; i < len; i++)
-		prod *= (signed char)name[i];
 	key[2] = charset[(prod ^ 0x55) & 0x3f];
-
-	for (max = (signed char)name[0], i = 0; i < len; i++)
-		if ((signed char)name[i] > max)
-			max = (signed char)name[i];
 	srand(max ^ 0xe);
 	key[3] = charset[rand() & 0x3f];
-
-	for (sq = 0, i = 0; i < len; i++)
-		sq += (signed char)name[i] * (signed char)name[i];
 	key[4] = charset[(sq ^ 0xef) & 0x3f];
-
-	for (r = 0, i = 0; (signed char)name[0] > i; i++)
-		r = rand();
-	key[5] = charset[(r ^ 0xe5) & 0x3f];
-
+	key[5] = charset[(burn_rand((signed char)name[0]) ^ 0xe5) & 0x3f];
 	key[6] = '\0';
+
 	printf("%s", key);
 	return (0);
 }
