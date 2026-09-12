af83c770d886796ffc5154be579bddbc3e0b5298
Egi-de <egidefiston@gmail.com>
2026-09-12 15:06:42 +0200

    Add singly linked lists tasks

---
+++ singly_linked_lists/101-hello_alx.asm
@@ -0,0 +1,16 @@
+section .data
+    msg db "Hello, Frontier", 10, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+    lea rdi, [rel msg]
+    xor eax, eax
+    call printf
+    mov eax, 0
+    pop rbp
+    ret
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+# Singly Linked Lists
+
+## Description
+
+This project covers singly linked lists in C.
+
+## Author
+Egi-de
