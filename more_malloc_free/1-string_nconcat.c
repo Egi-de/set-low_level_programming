91e51d21164743f5f91a7f81fcfb901e1334e4b5
Egi-de <egidefiston@gmail.com>
2026-09-12 14:51:02 +0200

    Add more_malloc_free tasks

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates s1 with the first n bytes of s2
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to use from s2
+ * Return: pointer to newly allocated concatenated string, or NULL on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *result;
+	unsigned int len1, len2, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+	result = malloc(sizeof(char) * (len1 + n + 1));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
+	result[len1 + n] = '\0';
+
+	return (result);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,8 @@
+# More malloc, free
+
+## Description
+
+This project covers more dynamic memory allocation techniques in C.
+
+## Author
+Egi-de
