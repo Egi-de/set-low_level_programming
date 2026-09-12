848efad7ecdd66987de6046a8fad5e29d7b2b42b
Egi-de <egidefiston@gmail.com>
2026-09-12 15:16:35 +0200

    Replace strlen with manual loop to satisfy ltrace allowed functions

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -23,7 +24,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
 	new->next = *head;
 	*head = new;
 
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *tmp;
+	unsigned int len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -24,7 +25,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
